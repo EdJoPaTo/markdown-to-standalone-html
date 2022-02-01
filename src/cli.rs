f466044b72052a8fd08ef179a9f333ad3b38afd7
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2022-02-01 11:33:21 +0100

    refactor: update to clap v3

--- src/cli.rs
+++ src/cli.rs
@@ -1,44 +1,49 @@
-use clap::{App, AppSettings, Arg, SubCommand};
+use clap::{app_from_crate, App, AppSettings, Arg};
 
 #[must_use]
-pub fn build() -> App<'static, 'static> {
-    let markdown_file = Arg::with_name("markdown-file")
+pub fn build() -> App<'static> {
+    let markdown_file = Arg::new("markdown-file")
         .value_name("FILE")
         .takes_value(true)
         .required(true)
         .help("Markdown file to be parsed. Use - to read from stdin instead.");
 
-    App::new("Markdown to Standalone HTML")
+    app_from_crate!()
+        .name("Markdown to Standalone HTML")
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
-        .global_setting(AppSettings::ColoredHelp)
         .setting(AppSettings::SubcommandsNegateReqs)
         .subcommand(
-            SubCommand::with_name("template").about("Print the included template to stdout."),
+            App::new("template").about("Print the included template to stdout."),
         )
         .subcommand(
-            SubCommand::with_name("raw")
+            App::new("raw")
                 .about("Only parse the markdown to html without any further modifications")
                 .arg(&markdown_file),
         )
         .arg(
-            Arg::with_name("template-file")
+            Arg::new("template-file")
                 .long("template")
-                .short("t")
+                .short('t')
                 .value_name("FILE")
                 .takes_value(true)
                 .help("Template file to be used instead of the builtin one.")
                 .long_help("Template file to be used instead of the builtin one. Use the subcommand template to print the builtin template to stdout."),
         )
         .arg(
-            Arg::with_name("no-inline")
+            Arg::new("no-inline")
                 .long("no-inline")
-                .short("i")
+                .short('i')
                 .help("Don't try to inline assets.")
                 .long_help("Don't try to inline assets. Normally assets are inlined with monolith. When monolith is not in PATH a warning is shown. This warning is also suppressed with this flag."),
         )
         .arg(
             &markdown_file,
         )
+}
+
+#[test]
+fn verify_app() {
+    build().debug_assert();
 }
